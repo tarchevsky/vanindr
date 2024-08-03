593bd92740315081ad4181660f684b3ed0f425c0
tarchevsky <i.tarchevsky@gmai.com>
2024-08-03 20:38:49 +0300

    Отступ снизу footerGrid в мобильных убран

--- src/components/footerGrid/FooterGrid.tsx
+++ src/components/footerGrid/FooterGrid.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image'
 
 const FooterGrid = () => {
 	return (
-		<div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-4 mb-10'>
+		<div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-4 lg:mb-10'>
 			<Image
 				src='/footer-grid-1.png'
 				width={500}
