8730daa77ee632e7ea7ab13ed420ba3f0775f120
tarchevsky <i.tarchevsky@gmai.com>
2024-08-03 19:24:54 +0300

    Добавлен замечательный новый блок Friends, который берёт из массива имена и подставляет их в адреса картинок, альты и gridArea. Заодно переместил картинки по друзьям в отдельную папку friends в public

--- src/components/friends/Friends.tsx
+++ src/components/friends/Friends.tsx
@@ -1,10 +1,57 @@
 import cn from 'clsx'
 import styles from './Friends.module.scss'
+import Image from 'next/image'
 
 const Friends = () => {
+	const friendsData = [
+		{ person: 'ivan' },
+		{ person: 'masha' },
+		{ person: 'lesya' },
+		{ person: 'sukhrob' },
+		{ person: 'nikita' },
+		{ person: 'natasha2' },
+		{ person: 'serj' },
+		{ person: 'olga' },
+		{ person: 'evgenia' },
+		{ person: 'mama' },
+		{ person: 'natasha1' },
+		{ person: 'fedor' },
+		{ person: 'mark' },
+		{ person: 'galina' },
+		{ person: 'dunya' }
+	]
+
 	return (
-		<div className={cn(styles.friends, 'relative grid')}>
-			<div></div>
+		<div className='relative mb-20'>
+			<h3 className='text-4xl font-bold text-center mb-[51px]'>Туристы</h3>
+			<div
+				className={cn(
+					styles.friends,
+					'relative grid gap-y-20 gap-x-6 md:max-w-2xl m-auto mb-16'
+				)}
+			>
+				{friendsData.map((friend, index) => (
+					<Image
+						key={index}
+						src={`/friends/${friend.person}.png`}
+						width={204}
+						height={204}
+						alt={friend.person}
+						style={{ gridArea: friend.person }}
+						className={cn(
+							'p-2 border border-black rounded-full',
+							`${friend.person === 'natasha1' ? 'justify-self-center self-center w-[100px] h-[100px] xs:w-[130px] xs:h-[130px] sm:w-[150px] sm:h-[150px]' : ''}`
+						)}
+					/>
+				))}
+			</div>
+			<Image
+				src='/friends-bg-full.png'
+				width={1900}
+				height={800}
+				alt='Фон блока про друзей'
+				className='hidden lg:block absolute -z-10 top-44 left-0 w-full'
+			/>
 		</div>
 	)
 }
